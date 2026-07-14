d114eeac95e3d58830c688453a210c4a97125a4d
J.K.D.N.Manusha <2023s20074@stu.cmb.ac.lk>
2026-07-14 17:54:49 +0530

    completed manual code

--- Version_A_traditional/StudentLearningSystem.java
+++ Version_A_traditional/StudentLearningSystem.java
@@ -37,7 +37,64 @@ public class StudentLearningSystem{
             System.out.println("04. Exit");
             System.out.println("\n\nEnter your choice");
             int choice = sc.nextInt();
-   
+
+            switch(choice){
+                case 1:
+                    if(count< students.length){
+                        System.out.println("Enter ID:");
+                        int id = sc.nextInt();
+                        sc.nextLine();
+                        System.out.println("Enter Name: ");
+                        String name = sc.nextLine();
+                        System.out.println("Enter Subject:");
+                        String subject = sc.nextLine();
+                        System.out.println("Enter Marks:");
+                        double marks = sc.nextDouble();
+
+                        students[count]= new Student(id, name, subject, marks);
+                        count++;
+                        System.out.println("Student added Successfully!");
+
+
+                    }else{
+                        System.out.println("Student List if full");
+                    }
+                    break;
+
+                    case 2:
+                        System.out.println("\n--Student List--");
+                        for(int i=0;i<count;i++){
+                            students[i].display();
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("Enter ID to search:");
+                        int searchId = sc.nextInt();
+
+                        for(int i = 0; i<count; i++){
+                            if(students[i].id == searchId){
+                                System.out.println("Student Found\n");
+                                students[i].display();
+                                break;
+                            }
+                            if(i==count-1){
+                                System.out.println("Student with ID: " +searchId + "not found.");
+
+                            }
+                        }
+                        break;
+
+                        case 4:
+                            System.out.println("Exiting...");
+                            sc.close();
+                            return;
+                        
+                         default:
+                            System.out.println("Invalid choice! Try again.");
+
+            }
         }
     }
 }
+
